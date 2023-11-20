dd75dc12e6071a805db737b98ad6903bdd1aa709
Ryan Barillos <129530399+ryanbarillos@users.noreply.github.com>
2023-11-20 11:29:25 -0800

    Added comment on authors

--- SODV2101-FinalProject/Program.cs
+++ SODV2101-FinalProject/Program.cs
@@ -1,3 +1,10 @@
+/*
+ * SODV2101 Final Project
+ * By Ryan Barillos, Nathaniel Gatus, Emmanuel Pagaduan
+ * 
+ * Date Started: 20 Nov 2023
+ * Day Finished: 01 Dec 2023
+ */
 namespace SODV2101_FinalProject
 {
     internal static class Program
